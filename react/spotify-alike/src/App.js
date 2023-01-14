7bee57ad2ec68961d3f4a72001462166850d8214
Daria Babchenko <dashiko.tym@gmail.com>
2023-01-14 14:58:53 +0300

    done-side-bar

--- react/spotify-alike/src/App.js
+++ react/spotify-alike/src/App.js
@@ -14,22 +14,19 @@ function App() {
         <div className="main-nav">
           <div className='home'>
             <a href="#">
-              {/* <img src="./home-svgrepo-com.svg" alt="home" /> */}
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-              </svg>
+              <span class="icon-home ic"></span>
               <span>Home</span>
             </a>
           </div>
           <div className='search'>
             <a href="#">
-              <img src="./search-svgrepo-com.svg" alt="search" />
+              <span class="icon-search ic"></span>
               <span>Search</span>
             </a>
           </div>
           <div className='library'>
             <a href="#">
-              <img src="./library-svgrepo-com.svg" alt="library" />
+              <span class="icon-library ic"></span>
               <span>Your library</span>
             </a>
           </div>
@@ -37,19 +34,21 @@ function App() {
         <div className='dop-nav'>
           <div className='playlist'>
             <a href="#">
-              <img src="./add-svgrepo-com.svg" alt="add playlist" />
+              <span class="icon-playlist ic"></span>
               <span>Create Playlist</span>
             </a>
           </div>
           <div className='liked-songs'>
             <a href="#">
-              <img src="./heart-like-svgrepo-com.svg" alt="liked songs" />
+              <span class="icon-heart ic"></span>
               <span>Liked Songs</span>
             </a>
           </div>
         </div>
       </nav>
-      <main className="main">main</main>
+      <main className="main">main
+        <span class="icon-heart"></span>
+      </main>
       <footer className="footer">footer</footer>
     </div>
 
