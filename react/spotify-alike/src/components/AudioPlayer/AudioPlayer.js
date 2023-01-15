c78670882ed90d44e347f465809f9be7859f7969
Seva <sevauni@gmail.com>
2023-01-15 08:22:19 +0200

    try audio comp

--- react/spotify-alike/src/components/AudioPlayer/AudioPlayer.js
+++ react/spotify-alike/src/components/AudioPlayer/AudioPlayer.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 
+import ReactAudioPlayer from 'react-audio-player';
 
 import "./AudioPlayer.css";
 
@@ -12,11 +13,20 @@ import "./AudioPlayer.css";
 class AudioPlayer extends Component {
     state = {
         playing: false,
-        volume: 70,
+        volume: 1,
         currentTrack: null,
         currentTrackLength: 500,
         currentTrackPlayPosition: 0,
     };
+
+    onSongLoaded = (song) => {
+        console.log(this.rap.audioEl);
+        song = Math.floor(song.current.duration);
+        console.log(song);
+        this.setState({
+            currentTrackLength: song
+        });
+    }
 
 
     testClick = (input) => {
@@ -28,6 +38,9 @@ class AudioPlayer extends Component {
         console.log("click");
     }
 
+    componentDidMount() {
+
+    }
 
     render() {
         return (
@@ -36,7 +49,7 @@ class AudioPlayer extends Component {
                     <span id="current-time" className='time'>0:00</span>
                     <Button onClick={this.clickTest} className="buttons-spacing-test" variant="secondary">Shuffle</Button>
                     <Button onClick={this.clickTest} className="buttons-spacing-test" variant="secondary">Prev</Button>
-                    <Button onClick={this.clickTest} className="buttons-spacing-test" variant="secondary">Play</Button>
+                    <Button onClick={() => {}} className="buttons-spacing-test" variant="secondary">Play</Button>
                     <Button onClick={this.clickTest} className="buttons-spacing-test" variant="secondary">Next</Button>
                     <Button onClick={this.clickTest} className="buttons-spacing-test" variant="secondary">Enable Repeat</Button>
                     <span id="duration" className='time'>0:00</span>
@@ -56,6 +69,17 @@ class AudioPlayer extends Component {
                         rangeSlideDisabled={true}
                         onInput={(value) => { this.testClick(value) }}
                     />
+                    <ReactAudioPlayer
+                        src={"music.mp3"}
+                        controls
+                        volume={this.state.volume}  
+                        ref={(element) => { this.rap = element; }}
+                        onLoadedMetadata={() => {
+                            console.log("Info loaded");
+                            this.onSongLoaded(this.rap.audioEl)
+                        }}
+
+                    />
                 </div>
             </div>
         );
@@ -66,6 +90,6 @@ class AudioPlayer extends Component {
 }
 
 
-
+//this.rap.audioEl.current.duration
 
 export default AudioPlayer;
