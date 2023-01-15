91bfc42df0bd54e002c8ceec3faa16c8b5a41f97
Seva <sevauni@gmail.com>
2023-01-15 17:35:37 +0200

    before merge

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import Footer from './components/Footer/Footer';
+
 function App() {
   return (
 
@@ -59,35 +61,7 @@ function App() {
         <span className="icon-like">sdsd</span>
         <span className="icon-shuffle-1-svgrepo-com"></span>
       </main>
-      <footer className="footer">
-        <div className='left-part'>
-          <div className='current-song'></div>
-          <div className='song-title'>Song Title</div>
-          <div className='band-name'>Band Name</div>
-          <span className="icon-like"></span>
-        </div>
-        <div className='center-part'>
-          <span className="icon-shuffle-1-svgrepo-com"></span>
-          <span className="icon-play-and-pause"></span>
-          <span className="icon-play-circle"></span>
-          <span className="icon-play-and-pause2"></span>
-          <span className="icon-repeat-one"></span>
-          <div id="music-line">
-            0:18
-            <div className="line"></div>
-            3:15
-          </div>
-        </div>
-        <div className='right-part'>
-          <span className="icon-microphone ir"></span>
-          <span className="icon-queue ir"></span>
-          <span className="icon-laptop ir"></span>
-          <div className='sound'>
-            <span id='sound-mute' className="icon-sound-min"></span>
-            <div className='sound-line'></div>
-          </div>
-        </div>
-      </footer>
+      <Footer></Footer>
     </div>
 
 
--- music-app/src/components/Footer/Footer.js
+++ music-app/src/components/Footer/Footer.js
@@ -5,7 +5,35 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div></div>
+            <footer className="footer">
+                <div className='left-part'>
+                    <div className='current-song'></div>
+                    <div className='song-title'>Song Title</div>
+                    <div className='band-name'>Band Name</div>
+                    <span className="icon-like"></span>
+                </div>
+                <div className='center-part'>
+                    <span className="icon-shuffle-1-svgrepo-com"></span>
+                    <span className="icon-play-and-pause"></span>
+                    <span className="icon-play-circle"></span>
+                    <span className="icon-play-and-pause2"></span>
+                    <span className="icon-repeat-one"></span>
+                    <div id="music-line">
+                        0:18
+                        <div className="line"></div>
+                        3:15
+                    </div>
+                </div>
+                <div className='right-part'>
+                    <span className="icon-microphone ir"></span>
+                    <span className="icon-queue ir"></span>
+                    <span className="icon-laptop ir"></span>
+                    <div className='sound'>
+                        <span id='sound-mute' className="icon-sound-min"></span>
+                        <div className='sound-line'></div>
+                    </div>
+                </div>
+            </footer>
         );
     }
 }
