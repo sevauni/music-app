326ef7e2e962b92354d5c32b50974ecda7ae29d6
Seva <sevauni@gmail.com>
2023-01-15 09:23:21 +0200

    merged

--- react/spotify-alike/src/index.js
+++ react/spotify-alike/src/index.js
@@ -6,6 +6,7 @@ import AudioPlayer from './components/AudioPlayer/AudioPlayer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AudioPlayer  />
   </React.StrictMode>
 );
