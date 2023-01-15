9953b21796dee0f741162ff8667633d11a244474
Seva <sevauni@gmail.com>
2023-01-15 13:47:39 +0200

    before react sound

--- react/spotify-alike/src/components/AudioPlayer/AudioPlayer.js
+++ react/spotify-alike/src/components/AudioPlayer/AudioPlayer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 
-import Audio from 'ts-audio';
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 
@@ -26,14 +25,6 @@ class AudioPlayer extends Component {
     }
 
 
-    // onSongLoaded = (song) => {
-    //     console.log(this.rap.audioEl);
-    //     song = Math.floor(song.current.duration);
-    //     console.log(song);
-    //     this.setState({
-    //         currentTrackLength: song
-    //     });
-    // }
 
     _onVolumeUpdate = (input) => {
 
@@ -58,13 +49,6 @@ class AudioPlayer extends Component {
         console.log("click");
     }
 
-    player = Audio({
-        file: './music.mp3',
-        loop: false,
-        volume: this.state.volume,
-        autoPlay: false,
-        preload: true,
-    });
 
     componentDidMount() {
 
@@ -79,27 +63,22 @@ class AudioPlayer extends Component {
 
                     <Button onClick={this.clickTest} className="buttons-spacing-test" variant="secondary">Shuffle</Button>
 
-                    <Button onClick={() => {
-                        console.log(this.player.audioCtx);
-                        console.log(this.player.audioCtx.currentTime);
-                        this.player.setValueAtTime(33);
+                    <Button onClick={() => { }}
+                        className="buttons-spacing-test" variant="secondary">Prev</Button>
 
-                        this.player.audioCtx.currentTime = 8;
-                    }}
-                     className="buttons-spacing-test" variant="secondary">Prev</Button>
-
-                    <Button onClick={() => {
-                        this.player.toggle();
-                        console.log(this.player.volume);
-
-                    }} className="buttons-spacing-test" variant="secondary">Play</Button>
+                    <Button onClick={() => { }}
+                        className="buttons-spacing-test" variant="secondary">Play</Button>
                     <Button onClick={this.clickTest} className="buttons-spacing-test" variant="secondary">Next</Button>
+
+
                     <Button
                         onClick={this.clickTest}
                         className="buttons-spacing-test"
                         variant="secondary"
                     >Enable Repeat</Button>
+
                     <span id="duration" className='time'>0:00</span>
+
                     <RangeSlider
                         className="slider slider-track"
                         defaultValue={[0, 0]}
@@ -108,19 +87,20 @@ class AudioPlayer extends Component {
                         onInput={(value) => { this.testClick(value) }}
                         max={this.state.currentTrackLength}
                     />
+
                     <Button
                         onClick={() => { this.player.volume = 0 }}
                         className="buttons-spacing-test"
                         variant="secondary"
-                    >Mute</Button>
+                    >Mute
+                    </Button>
+
                     <RangeSlider
                         className="slider slider-volume"
                         defaultValue={[0, this.state.volume * 100]}
                         thumbsDisabled={[true, false]}
                         rangeSlideDisabled={true}
                         onInput={(value) => {
-                            //this.testClick(value);
-                            this.player.volume = this._onVolumeUpdate(value[1]);
                             console.log(this.player.volume);
                         }}
                     />
